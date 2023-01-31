166c290fe12192bf1b51d17b3b0e9c3b983fc31d
Kripton0810 <subhankar0810@gmail.com>
2023-02-01 00:01:33 +0530

    updates

--- src/components/principalmessage.jsx
+++ src/components/principalmessage.jsx
@@ -1,23 +1,56 @@
-import React from "react"
-import { Card } from "flowbite-react"
+import React, { useState } from "react"
+import principalMessage from "../data/principalLetter.js"
+import { Card, Button } from "flowbite-react"
+import principalPic from "../images/Principal.jpeg"
+import { Edit } from "@mui/icons-material"
+import { Modal } from "flowbite-react"
 
+function ReadMore(data) {
+  const [click, onClick] = useState(false)
+  return (
+    <React.Fragment>
+      <Button onClick={() => onClick(true)}>
+        <Edit />
+        Read More{" "}
+      </Button>
+      <Modal
+        dismissible="true"
+        show={click}
+        size="5xl"
+        onClose={() => onClick(false)}
+        position="center"
+      >
+        <Modal.Header>Message from the Principal</Modal.Header>
+        <Modal.Body>
+          <img
+            src={principalPic}
+            className="mx-auto"
+            alt=""
+            width="300px"
+            height="300px"
+          />
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              {data.data.message}
+            </p>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </React.Fragment>
+  )
+}
 export default function PrincipalMessage() {
   return (
     <div className="max-w-lg">
-      <Card
-        horizontal={true}
-        imgSrc="http://aiwcaoe.edu.in/images/index/Principal.jpeg"
-        
-      >
+      <Card horizontal={true} imgSrc={principalPic}>
         <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           PRINCIPAL MESSAGE
         </h5>
         <p className="font-normal text-gray-700 dark:text-gray-400">
           Dear Parents, Greetings from AIWC family, we at AIWC-AOE, seek to
-          achieve excellence in all our pursuits. Bearing in mind the need to
-          maintain individual's identity each and every child under our care is
-          helped to develop to his/her fullest potential...
+          achieve excellence in all our pursuits...
         </p>
+        <ReadMore data={principalMessage} />
       </Card>
     </div>
   )
---
+++ src/data/principalLetter.js
@@ -0,0 +1,22 @@
+const principalMessage = {
+  message: `Dear Parents,
+
+Greetings from AIWC family, we at AIWC-AOE, seek to achieve excellence in all our pursuits. Bearing in mind the need to maintain individual's identity each and every child under our care is helped to develop to his/her fullest potential without constraining his/her inherent abilities. Keeping with our country's lofty ideals aand values we inculcate in our students a sense of national unity, integrity, love and universal brother hood and the child is thereby prepared to meet the challenges of life.
+
+In these days of intense competition, education is one of the major areas where special emphasis is laid, be it star achievements in all facets of school life or be it academics, cultural activities or games and sports. These students are urged to work hard and complete relentlessly.
+
+Dear parents, here is a caution. Please don't compare your child's performance with his/her peer group. He/She is God's unique and ultimate creation by himself/herself. For his/her all round development and healthy life, bear in mind his/her capabilities, aptitudes and simple desires, before setting their goals.
+
+We owe it to our society, our country and to the world at large that we mould these young impressionable minds into responsible citizens of tomorrow.
+
+Looking forward to your kind cooperation in this joint endeavour.
+
+With kind regards!
+Mrs. Jasbeer Kaur Gill
+Principal
+
+AIWC ACADEMY OF EXCELLENCE`,
+  imageUrl: "http://aiwcaoe.edu.in/images/index/Principal.jpeg",
+}
+
+export default principalMessage
