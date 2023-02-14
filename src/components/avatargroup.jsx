3f61750c1bbfa15df826510202edac0084c418d4
Kripton0810 <subhankar0810@gmail.com>
2023-02-15 01:34:06 +0530

    data part complete

---
+++ src/components/avatargroup.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Avatar } from "flowbite-react"
+
+function AvatarGroupMaker(picsData) {
+  return picsData.pics.map((item, index) => (
+    <Avatar img={item} rounded={true} stacked={true} />
+  ))
+}
+export default function AvatarGroup({ avatarData }) {
+  return (
+    <React.Fragment>
+      <Avatar.Group>
+        <AvatarGroupMaker pics={avatarData.toppersImagesUrl} />
+      </Avatar.Group>
+    </React.Fragment>
+  )
+}
---
+++ src/pages/achivers/avatardata.js
@@ -0,0 +1,73 @@
+const avatarData = [
+  {
+    year: 2023,
+    data: [
+      {
+        classGroup: 10,
+        year: 2023,
+        resultDate: "2023-05-10",
+        toppersImagesUrl: [
+          "http://aiwcaoe.edu.in/images/sports/Kundan%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Nishant%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Jyoti%20Kumari%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Pramod%20Giri%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Rajat%20Jha%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Kaushal%201.jpeg",
+        ],
+        batchDiscription:
+          "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      },
+      {
+        classGroup: 12,
+        year: 2023,
+        resultDate: "2023-05-10",
+        toppersImagesUrl: [
+          "http://aiwcaoe.edu.in/images/sports/Kundan%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Nishant%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Jyoti%20Kumari%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Pramod%20Giri%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Rajat%20Jha%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Kaushal%201.jpeg",
+        ],
+        batchDiscription:
+          "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      },
+    ],
+  },
+  {
+    year: 2022,
+    data: [
+      {
+        classGroup: 10,
+        year: 2022,
+        resultDate: "2022-05-10",
+        toppersImagesUrl: [
+          "http://aiwcaoe.edu.in/images/sports/Kundan%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Nishant%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Jyoti%20Kumari%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Pramod%20Giri%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Rajat%20Jha%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Kaushal%201.jpeg",
+        ],
+        batchDiscription:
+          "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      },
+      {
+        classGroup: 12,
+        year: 2022,
+        resultDate: "2023-06-10",
+        toppersImagesUrl: [
+          "http://aiwcaoe.edu.in/images/sports/Kundan%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Nishant%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Jyoti%20Kumari%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Pramod%20Giri%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Rajat%20Jha%201.jpeg",
+          "http://aiwcaoe.edu.in/images/sports/Kaushal%201.jpeg",
+        ],
+        batchDiscription:
+          "All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.",
+      },
+    ],
+  },
+]
+export default avatarData
